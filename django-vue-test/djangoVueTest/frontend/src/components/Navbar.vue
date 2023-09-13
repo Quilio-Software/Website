70443423160af113d56a74f81451937bf59c9a90
SolomonMoulangLewis <SOLOMON.LEWIS21@bathspa.ac.uk>
2023-09-13 11:39:18 -0400

    New styling for fullwidth navbar (test)

--- django-vue-test/djangoVueTest/frontend/src/components/Navbar.vue
+++ django-vue-test/djangoVueTest/frontend/src/components/Navbar.vue
@@ -53,10 +53,24 @@ onMounted(() => {
                     ease: 'Power2.easeInOut',
                 });
 
-                sideNavAnimateIn =
+                //SIDE NAV ORIGINAL ANIMATION
+                /* sideNavAnimateIn =
                     gsap.from(sideNav.value, {
                         opacity: 0,
                         height: '0%',
+                        duration: 0.5,
+                        ease: 'Power2.easeInOut',
+                        delay: 0.1,
+                    }); */
+
+                //SIDE NAV FULL WIDTH TEST ANIMATION
+                sideNavAnimateIn =
+                    gsap.fromTo(sideNav.value, {
+                        opacity: 0,
+                        height: 0,
+                    }, {
+                        opacity: 1,
+                        height: 'fit-content',
                         duration: 0.5,
                         ease: 'Power2.easeInOut',
                         delay: 0.1,
@@ -244,45 +258,93 @@ onMounted(() => {
             </ul>
         </nav>
 
-        <!-- SideNav -->
-        <div ref="sideNav" class="absolute right-0 h-5/6 w-2/5 bg-gray/900 bg-opacity-95">
-            <div class="ml-10 h-full flex flex-col gap-10">
+        <!-- SideNav FULL WIDTH TEST -->
+        <div ref="sideNav" class="absolute sm:hidden right-0 w-full bg-gray/900 bg-opacity-90">
+
+            <div class="ml-10 my-10 flex flex-col gap-10">
 
                 <!-- LINKS -->
-                <div class="mt-8 h-2/5 flex flex-col justify-around">
+                <div class="flex flex-col gap-10">
                     <!-- PRODUCTS -->
                     <RouterLink to="/products/deals"
-                        class="text-lg font-poppins font-normal tracking-wide text-gray/50 hover:underline ease duration-300">
+                        class="text-lg font-poppins font-normal tracking-wide text-gray/50 hover:text-primary/400 ease duration-300">
                         Products
                     </RouterLink>
 
                     <!-- ABOUT US -->
                     <RouterLink to="/about-us"
-                        class="text-lg font-poppins font-normal tracking-wide text-gray/50 hover:underline ease duration-300">
+                        class="text-lg font-poppins font-normal tracking-wide text-gray/50 hover:text-primary/400 ease duration-300">
                         About Us
                     </RouterLink>
 
                     <!-- CONTACT US -->
+                    <RouterLink to="/contact-us"
+                        class="text-lg font-poppins font-normal tracking-wide text-gray/50 hover:text-primary/400 ease duration-300">
+                        Contact Us
+                    </RouterLink>
+                </div>
+
+                <div class="hidden">
+                    <!-- DIVIDER -->
+                    <div class="h-[1px] w-3/5 rounded-full bg-gray/600"></div>
+
+                    <!-- FOOTER -->
+                    <p class=" w-full xl:w-2/3 font-poppins text-sm text-gray/600">
+                        © 2023
+                        Quilio Ltd.</p>
+
+                    <!-- SOCIALS -->
+                    <div class="flex gap-4">
+                        <img class="w-6" src="../assets/img/footer/socials/linkedin-logo.png" alt="">
+                        <img class="w-6" src="../assets/img/footer/socials/linkedin-logo.png" alt="">
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+
+        <!-- 
+        <!~~ SideNav ORIGINAL ~~>
+        <div ref="sideNav" class="absolute sm:hidden right-0 h-5/6 w-2/5 bg-gray/900 bg-opacity-95">
+            <div class="ml-10 h-full flex flex-col gap-10">
+
+                <!~~ LINKS ~~>
+                <div class="mt-8 h-2/5 flex flex-col justify-around">
+                    <!~~ PRODUCTS ~~>
+                    <RouterLink to="/products/deals"
+                        class="text-lg font-poppins font-normal tracking-wide text-gray/50 hover:underline ease duration-300">
+                        Products
+                    </RouterLink>
+
+                    <!~~ ABOUT US ~~>
+                    <RouterLink to="/about-us"
+                        class="text-lg font-poppins font-normal tracking-wide text-gray/50 hover:underline ease duration-300">
+                        About Us
+                    </RouterLink>
+
+                    <!~~ CONTACT US ~~>
                     <RouterLink to="/contact-us"
                         class="text-lg font-poppins font-normal tracking-wide text-gray/50 hover:underline ease duration-300">
                         Contact Us
                     </RouterLink>
                 </div>
 
-                <!-- DIVIDER -->
+                <!~~ DIVIDER ~~>
                 <div class="h-[1px] w-3/5 rounded-full bg-gray/600"></div>
 
-                <!-- FOOTER -->
+                <!~~ FOOTER ~~>
                 <p class=" w-full xl:w-2/3 font-poppins text-sm text-gray/600">
                     © 2023
                     Quilio Ltd.</p>
 
-                <!-- SOCIALS -->
+                <!~~ SOCIALS ~~>
                 <div class="flex gap-4">
                     <img class="w-6" src="../assets/img/footer/socials/linkedin-logo.png" alt="">
                     <img class="w-6" src="../assets/img/footer/socials/linkedin-logo.png" alt="">
                 </div>
             </div>
         </div>
+         -->
     </div>
 </template>
